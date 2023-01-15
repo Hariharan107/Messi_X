5c2dc7a8d3e54fb4fa249d056c514d22c2350dd7
Hariharan <hariharen107@gmail.com>
2023-01-15 18:27:20 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -62,7 +62,7 @@ const submitHandler = async (e) => {
     loader(messageDiv)
 
 
-    const response=await fetch("http://localhost:5000/",{
+    const response=await fetch("https://messi-x.onrender.com",{
         method:'POST',
         headers:{
             "Content-Type":"application/json"
